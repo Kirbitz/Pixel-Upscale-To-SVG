037249bd088590c7d63959c7d0b7eac37b7233c3
kirbitz <tyler.marefke@gmail.com>
2023-04-22 20:01:12 -0500

    merged main

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import cv2
-import numpy as np
 import up_scale_algs as usa
+import svg as svg
 
 gui_name = "Image Window"
+<<<<<<< HEAD
 img = cv2.imread("pixel_art/fire_emblem_character_portraits.png", cv2.IMREAD_COLOR)
+=======
+
+img_name = "octopath_traveler_characters"
+
+ANIMATED = False
+file_path = f"pixel_art/{img_name}.png"
+SPRITE_WIDTH, SPRITE_HEIGHT = 16, 16
+FPS = 5
+FRAME_TIME = 1000 // FPS # milliseconds per frame of animation
+currentFrame = -1
+
+img = cv2.imread(file_path, cv2.IMREAD_COLOR)
+img_show = img.copy()
+full_img = img.copy()
+
+sprites_per_row = img.shape[1] // SPRITE_WIDTH
+sprites_per_column = img.shape[0] // SPRITE_HEIGHT
+
+>>>>>>> main
 scale = 1
 scale_type = 0
 
-def image_upscale(x):
+def update_scale_type(x):
     global scale_type
     scale_type = x
+    update_image_upscale()
 
+def update_image_frame(frame_increment = 1):
+    global full_img, img, currentFrame
+    currentFrame += frame_increment
+    currentFrame = currentFrame % (sprites_per_row * sprites_per_column)
+
+    img = full_img[(currentFrame // sprites_per_row) * SPRITE_HEIGHT:((currentFrame // sprites_per_row) + 1) * SPRITE_HEIGHT, 
+                   (currentFrame % sprites_per_row) * SPRITE_WIDTH:((currentFrame % sprites_per_row) + 1) * SPRITE_WIDTH]
+
+    update_image_upscale()
+
+def update_image_upscale():
+    global img_show
     if scale_type == 0:
         img_show = usa.nearest_neighbor(img, 2 ** scale)
     elif scale_type == 1:
@@ -24,13 +57,39 @@ def image_upscale(x):
     
     cv2.imshow(gui_name, img_show)
 
+def create_svg(x):
+    global img_show
+    if x == 1:
+        svg.convert_to_svg(img_show, f'svg/{img_name}.svg')
+
 def main():
     cv2.namedWindow(gui_name)
+<<<<<<< HEAD
     cv2.createTrackbar('Scale Type', gui_name, 0, 4, image_upscale)
     image_upscale(0)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+=======
+    global img
+>>>>>>> main
     
+    if not ANIMATED:
+        update_image_upscale()
+        cv2.createTrackbar('Scale Type', gui_name, 0, 4, update_scale_type) # create the trackbar after showing the image to ensure trackbar fits within window
+        cv2.createTrackbar("Create SVG", gui_name, 0, 1, create_svg)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return
+    
+    update_image_frame(0)
+    cv2.createTrackbar('Scale Type', gui_name, 0, 4, update_scale_type)
+
+    # animation loop (runs while main window is open)
+    while (cv2.getWindowProperty(gui_name, cv2.WND_PROP_VISIBLE) == 1):
+        update_image_frame()
+        cv2.waitKey(FRAME_TIME)
+        
+
 
 if __name__ == '__main__':
     main()
---
+++ svg.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+def convert_to_svg(img, path):
+    height, width = img.shape[:2]
+    most_pixels = ""
+    max_list = 0
+    colors = {}
+
+    for i in range(height):
+        for j in range(width):
+            key = f"#{img[i, j, 2]:02x}{img[i, j, 1]:02x}{img[i, j, 0]:02x}"
+            if key in colors.keys():
+                colors[key].append([i, j])
+            else:
+                colors[key] = [[i, j]]
+
+    for key in colors.keys():
+        colors[key].append([-1, -1])
+        if max_list < len(colors[key]):
+            max_list = len(colors[key])
+            most_pixels = key
+
+    svg_file = open(path, 'w+')
+    svg_file.write(f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 {str(width)} {str(height)}" shape-rending="crispEdges">')
+    
+    for color in colors.keys():
+        # This is a check to see if there is a background of pixels that can be removed from the svg render
+        # The threshold value can be modified but 50% seems to be sufficient for now
+        if most_pixels == color and len(colors[color]) / (width * height) > 0.5:
+            continue
+        svg_file.write(f'<path stroke="{color}" d="')
+        current = colors[color][0]
+        w = 0
+        for x_y in colors[color]:
+            if current[1] == x_y[1] and x_y[0] == (current[0] + w):
+                w += 1
+            else:
+                svg_file.write(f"M{str(current[1])} {str(current[0])}h{str(w)}")
+                w = 1
+                current = x_y
+        svg_file.write('" />')
+
+    svg_file.write("</svg>")
+    svg_file.close()
