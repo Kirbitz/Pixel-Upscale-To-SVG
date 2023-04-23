e0a7e479b2ed69b6c32c9d00ea99b5a88da99943
kirbitz <tyler.marefke@gmail.com>
2023-04-23 18:46:49 -0500

    Formatted files based on flake8 recommendations

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ ANIMATED = False
 file_path = f"pixel_art/{img_name}.png"
 SPRITE_WIDTH, SPRITE_HEIGHT = 16, 16
 FPS = 5
-FRAME_TIME = 1000 // FPS # milliseconds per frame of animation
+FRAME_TIME = 1000 // FPS  # milliseconds per frame of animation
 currentFrame = -1
 
 img = cv2.imread(file_path, cv2.IMREAD_COLOR)
@@ -23,25 +23,34 @@ sprites_per_column = img.shape[0] // SPRITE_HEIGHT
 scale = 1
 scale_type = 0
 
+
 def update_scale_type(x):
     global scale_type
     scale_type = x
     update_image_upscale()
 
-def update_image_frame(frame_increment = 1):
+
+def update_image_frame(frame_increment=1):
     global full_img, img, currentFrame
     currentFrame += frame_increment
     currentFrame = currentFrame % (sprites_per_row * sprites_per_column)
 
-    img = full_img[(currentFrame // sprites_per_row) * SPRITE_HEIGHT:((currentFrame // sprites_per_row) + 1) * SPRITE_HEIGHT, 
-                   (currentFrame % sprites_per_row) * SPRITE_WIDTH:((currentFrame % sprites_per_row) + 1) * SPRITE_WIDTH]
+    img = full_img[
+        (currentFrame // sprites_per_row)
+        * SPRITE_HEIGHT : ((currentFrame // sprites_per_row) + 1)
+        * SPRITE_HEIGHT,
+        (currentFrame % sprites_per_row)
+        * SPRITE_WIDTH : ((currentFrame % sprites_per_row) + 1)
+        * SPRITE_WIDTH,
+    ]
 
     update_image_upscale()
+
 
 def update_image_upscale():
     global img_show
     if scale_type == 0:
-        img_show = usa.nearest_neighbor(img, 2 ** scale)
+        img_show = usa.nearest_neighbor(img, 2**scale)
     elif scale_type == 1:
         img_show = usa.EPX(img, Iterations=scale)
     elif scale_type == 2:
@@ -49,36 +58,39 @@ def update_image_upscale():
     elif scale_type == 3:
         img_show = usa.eagle_2x(img, Iterations=scale)
     elif scale_type == 4:
-        img_show = usa.bilinear(img, 2 ** scale)
-    
+        img_show = usa.bilinear(img, 2**scale)
+
     cv2.imshow(gui_name, img_show)
+
 
 def create_svg(x):
     global img_show
     if x == 1:
-        svg.convert_to_svg(img_show, f'svg/{img_name}.svg')
+        svg.convert_to_svg(img_show, f"svg/{img_name}.svg")
+
 
 def main():
     cv2.namedWindow(gui_name)
     global img
-    
+
     if not ANIMATED:
         update_image_upscale()
-        cv2.createTrackbar('Scale Type', gui_name, 0, 4, update_scale_type) # create the trackbar after showing the image to ensure trackbar fits within window
+        cv2.createTrackbar(
+            "Scale Type", gui_name, 0, 4, update_scale_type
+        )  # create the trackbar after showing the image to ensure trackbar fits within window
         cv2.createTrackbar("Create SVG", gui_name, 0, 1, create_svg)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         return
-    
+
     update_image_frame(0)
-    cv2.createTrackbar('Scale Type', gui_name, 0, 4, update_scale_type)
+    cv2.createTrackbar("Scale Type", gui_name, 0, 4, update_scale_type)
 
     # animation loop (runs while main window is open)
-    while (cv2.getWindowProperty(gui_name, cv2.WND_PROP_VISIBLE) == 1):
+    while cv2.getWindowProperty(gui_name, cv2.WND_PROP_VISIBLE) == 1:
         update_image_frame()
         cv2.waitKey(FRAME_TIME)
-        
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- up_scale_algs.py
+++ up_scale_algs.py
@@ -1,14 +1,24 @@
 import numpy as np
 
+
 def three_or_more_equal(a, b, c, d):
-    return np.logical_or(np.logical_and(np.all(a == b, axis=2), np.logical_or(np.all(a == c, axis=2), np.all(a == d, axis=2))), np.logical_and(np.all(a == c, axis=2), np.all(a == d, axis=2)), np.logical_and(np.all(b == c, axis=2), np.all(b == d, axis=2)))
+    return np.logical_or(
+        np.logical_and(
+            np.all(a == b, axis=2),
+            np.logical_or(np.all(a == c, axis=2), np.all(a == d, axis=2)),
+        ),
+        np.logical_and(np.all(a == c, axis=2), np.all(a == d, axis=2)),
+        np.logical_and(np.all(b == c, axis=2), np.all(b == d, axis=2)),
+    )
+
 
 def nearest_neighbor(img, scale_factor):
     img = img.repeat(scale_factor, 1).repeat(scale_factor, 0)
     return img
 
+
 def EPX(img, Iterations=1):
-    for k in range(Iterations):
+    for _ in range(Iterations):
         img_scaled = img.repeat(2, 1).repeat(2, 0)
 
         a = img[0:-2, 1:-1]
@@ -26,12 +36,13 @@ def EPX(img, Iterations=1):
         img_scaled[2:-2:2, 3:-2:2][b_mask] = b[b_mask]
         img_scaled[3:-2:2, 2:-2:2][c_mask] = c[c_mask]
         img_scaled[3:-2:2, 3:-2:2][d_mask] = d[d_mask]
-        
+
         img = np.array(img_scaled, dtype=np.uint8)
     return img
 
+
 def scale_2x(img, Iterations=1):
-    for k in range(Iterations):
+    for _ in range(Iterations):
         img_scaled = img.repeat(2, 1).repeat(2, 0)
 
         a = img[0:-2, 1:-1]
@@ -39,21 +50,38 @@ def scale_2x(img, Iterations=1):
         c = img[1:-1, 0:-2]
         d = img[2:, 1:-1]
 
-        a_mask = np.logical_and(np.all(a == c, axis=2), np.logical_not(np.all(c == d, axis=2)), np.logical_not(np.all(a == b, axis=2)))
-        b_mask = np.logical_and(np.all(a == b, axis=2), np.logical_not(np.all(a == c, axis=2)), np.logical_not(np.all(b == d, axis=2)))
-        c_mask = np.logical_and(np.all(d == c, axis=2), np.logical_not(np.all(d == b, axis=2)), np.logical_not(np.all(a == c, axis=2)))
-        d_mask = np.logical_and(np.all(b == d, axis=2), np.logical_not(np.all(b == a, axis=2)), np.logical_not(np.all(d == c, axis=2)))
+        a_mask = np.logical_and(
+            np.all(a == c, axis=2),
+            np.logical_not(np.all(c == d, axis=2)),
+            np.logical_not(np.all(a == b, axis=2)),
+        )
+        b_mask = np.logical_and(
+            np.all(a == b, axis=2),
+            np.logical_not(np.all(a == c, axis=2)),
+            np.logical_not(np.all(b == d, axis=2)),
+        )
+        c_mask = np.logical_and(
+            np.all(d == c, axis=2),
+            np.logical_not(np.all(d == b, axis=2)),
+            np.logical_not(np.all(a == c, axis=2)),
+        )
+        d_mask = np.logical_and(
+            np.all(b == d, axis=2),
+            np.logical_not(np.all(b == a, axis=2)),
+            np.logical_not(np.all(d == c, axis=2)),
+        )
 
         img_scaled[2:-2:2, 2:-2:2][a_mask] = a[a_mask]
         img_scaled[2:-2:2, 3:-2:2][b_mask] = b[b_mask]
         img_scaled[3:-2:2, 2:-2:2][c_mask] = c[c_mask]
         img_scaled[3:-2:2, 3:-2:2][d_mask] = d[d_mask]
-        
+
         img = np.array(img_scaled, dtype=np.uint8)
     return img
 
+
 def eagle_2x(img, Iterations=1):
-    for k in range(Iterations):
+    for _ in range(Iterations):
         img_scaled = img.repeat(2, 1).repeat(2, 0)
 
         s = img[:-2, :-2]
@@ -67,8 +95,12 @@ def eagle_2x(img, Iterations=1):
 
         top_left_mask = np.logical_and(np.all(v == s, axis=2), np.all(s == t, axis=2))
         top_right_mask = np.logical_and(np.all(t == u, axis=2), np.all(u == w, axis=2))
-        bottom_left_mask = np.logical_and(np.all(v == x, axis=2), np.all(x == y, axis=2))
-        bottom_right_mask = np.logical_and(np.all(w == z, axis=2), np.all(z == y, axis=2))
+        bottom_left_mask = np.logical_and(
+            np.all(v == x, axis=2), np.all(x == y, axis=2)
+        )
+        bottom_right_mask = np.logical_and(
+            np.all(w == z, axis=2), np.all(z == y, axis=2)
+        )
 
         img_scaled[2:-2:2, 2:-2:2][top_left_mask] = s[top_left_mask]
         img_scaled[2:-2:2, 3:-2:2][top_right_mask] = u[top_right_mask]
@@ -78,32 +110,61 @@ def eagle_2x(img, Iterations=1):
         img = np.array(img_scaled, dtype=np.uint8)
     return img
 
-def bilinear(img,scaleFactor): #There is a vectorized version of this that runs faster
+
+def bilinear(
+    img, scaleFactor
+):  # There is a vectorized version of this that runs faster
     height, width = img.shape[:2]
     height_scaled = height * scaleFactor
     width_scaled = width * scaleFactor
     img_scaled = np.zeros((height_scaled, width_scaled, 3))
     x_ratio = float(width - 1) / (width_scaled - 1)
-    y_ratio = float(height-1) / (height_scaled - 1)
+    y_ratio = float(height - 1) / (height_scaled - 1)
 
-    x1 = np.array([np.uint32(np.floor(x_ratio * np.arange(width_scaled)))]).repeat(height_scaled, axis=0)
-    y1 = np.uint32(np.floor(y_ratio * np.arange(height_scaled)[:, np.newaxis])).repeat(width_scaled, axis=1)
-    xh = np.array([np.uint32(np.ceil(x_ratio * np.arange(width_scaled)))]).repeat(height_scaled, axis=0)
-    yh = np.uint32(np.ceil(y_ratio * np.arange(height_scaled)[:, np.newaxis])).repeat(width_scaled, axis=1)
+    x1 = np.array([np.uint32(np.floor(x_ratio * np.arange(width_scaled)))]).repeat(
+        height_scaled, axis=0
+    )
+    y1 = np.uint32(np.floor(y_ratio * np.arange(height_scaled)[:, np.newaxis])).repeat(
+        width_scaled, axis=1
+    )
+    xh = np.array([np.uint32(np.ceil(x_ratio * np.arange(width_scaled)))]).repeat(
+        height_scaled, axis=0
+    )
+    yh = np.uint32(np.ceil(y_ratio * np.arange(height_scaled)[:, np.newaxis])).repeat(
+        width_scaled, axis=1
+    )
 
     a = img[np.ix_(y1[:, 0], x1[0])]
     b = img[np.ix_(y1[:, 0], xh[0])]
     c = img[np.ix_(yh[:, 0], x1[0])]
     d = img[np.ix_(yh[:, 0], xh[0])]
 
-    x_weight = np.array([(x_ratio * np.arange(width_scaled))]).repeat(height_scaled, axis=0) - x1
-    y_weight = (y_ratio * np.arange(height_scaled)[:, np.newaxis]).repeat(width_scaled, axis=1) - y1
+    x_weight = (
+        np.array([(x_ratio * np.arange(width_scaled))]).repeat(height_scaled, axis=0)
+        - x1
+    )
+    y_weight = (y_ratio * np.arange(height_scaled)[:, np.newaxis]).repeat(
+        width_scaled, axis=1
+    ) - y1
 
-    img_scaled[:, :, 0] = a[:, :, 0] * (1-x_weight) * (1-y_weight) + b[:, :, 0] * x_weight * (1-y_weight) + c[:, :, 0] * y_weight * (1-x_weight) + d[:, :, 0] * x_weight * y_weight
-    img_scaled[:, :, 1] = a[:, :, 1] * (1-x_weight) * (1-y_weight) + b[:, :, 1] * x_weight * (1-y_weight) + c[:, :, 1] * y_weight * (1-x_weight) + d[:, :, 1] * x_weight * y_weight
-    img_scaled[:, :, 2] = a[:, :, 2] * (1-x_weight) * (1-y_weight) + b[:, :, 2] * x_weight * (1-y_weight) + c[:, :, 2] * y_weight * (1-x_weight) + d[:, :, 2] * x_weight * y_weight
+    img_scaled[:, :, 0] = (
+        a[:, :, 0] * (1 - x_weight) * (1 - y_weight)
+        + b[:, :, 0] * x_weight * (1 - y_weight)
+        + c[:, :, 0] * y_weight * (1 - x_weight)
+        + d[:, :, 0] * x_weight * y_weight
+    )
+    img_scaled[:, :, 1] = (
+        a[:, :, 1] * (1 - x_weight) * (1 - y_weight)
+        + b[:, :, 1] * x_weight * (1 - y_weight)
+        + c[:, :, 1] * y_weight * (1 - x_weight)
+        + d[:, :, 1] * x_weight * y_weight
+    )
+    img_scaled[:, :, 2] = (
+        a[:, :, 2] * (1 - x_weight) * (1 - y_weight)
+        + b[:, :, 2] * x_weight * (1 - y_weight)
+        + c[:, :, 2] * y_weight * (1 - x_weight)
+        + d[:, :, 2] * x_weight * y_weight
+    )
 
     img = np.array(img_scaled, dtype=np.uint8)
     return img
-
-
